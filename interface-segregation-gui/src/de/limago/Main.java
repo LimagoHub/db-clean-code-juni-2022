cac05a3b448482d4a4b8519c539f5bb9ef50fbde
Jo Wagner <jowagner@limago.de>
2022-06-07 13:40:15 +0200

    demo

--- interface-segregation-gui/src/de/limago/Main.java
+++ interface-segregation-gui/src/de/limago/Main.java
@@ -1,15 +1,36 @@
 package de.limago;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Main extends Frame {
+public class Main extends Frame  {
 
 
     public Main()  {
+
         setSize(300, 300);
+        Button button = new Button("Drück mich");
+        button.addActionListener(e->beenden());
+        add(button);
+        this.addWindowListener(new MyWindowListener());
     }
 
     public static void main(String[] args) {
 	    new Main().setVisible(true);
+    }
+
+
+
+    private void beenden() {
+        // Speichern
+        dispose();
+    }
+
+    private class MyWindowListener extends WindowAdapter {
+        @Override
+        public void windowClosing(WindowEvent e) {
+           beenden();
+        }
     }
 }
